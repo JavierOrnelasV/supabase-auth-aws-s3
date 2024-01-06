21969812fc67c854e6308b3f5385969f23388952
Javier Ornelas <javier.ornelasv@gmail.com>
2024-01-06 13:48:57 -0600

    add region and finish get files endpoint

--- .aws/config.ts
+++ .aws/config.ts
@@ -2,7 +2,7 @@ import { S3Client } from "@aws-sdk/client-s3"
 import { fromIni } from "@aws-sdk/credential-provider-ini"
 import path from "path"
 
-// Set the path to your custom AWS credentials file
+// // Set the path to your custom AWS credentials file
 // process.env.AWS_SHARED_CREDENTIALS_FILE = path.join(
 //   __dirname,
 //   ".aws",
--- server/api/documents/index.get.ts
+++ server/api/documents/index.get.ts
@@ -2,17 +2,19 @@ import { s3Client } from "~/.aws/config"
 import { GetObjectCommand } from "@aws-sdk/client-s3"
 
 export default defineEventHandler(async (event) => {
-  const { file } = await readBody(event)
+  const { file } = getQuery(event)
 
   const downloadParams = {
     Bucket: "aws-woko",
-    Key: file,
+    Key: file as string,
   }
 
-  s3Client
+  return s3Client
     .send(new GetObjectCommand(downloadParams))
     .then((data) => {
-      console.log("File content:", data.Body?.toString())
+      console.log("File content in string:", data.Body?.toString())
+      console.log("File content:", data.Body)
+      return data.Body
     })
     .catch((err) => console.error(err))
 })
